a9df1a61ffa4490e4dfb2ed39b7fe19788e2303b
Jafaruz914 <programmeraziz5@gmail.com>
2025-09-20 12:02:40 +0500

    good

--- src/main/java/com/crm/auth/dto/LoginRequest.java
+++ src/main/java/com/crm/auth/dto/LoginRequest.java
@@ -2,9 +2,15 @@ package com.crm.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     @Email
     @NotBlank
